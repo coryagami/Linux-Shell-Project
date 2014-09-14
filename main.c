e7d02d6265b1667927267142ef846b5a51e65bd6
coryagami <ca11h@my.fsu.edu>
2014-09-14 14:22:59 -0400

    back up main

--- main.c
+++ main.c
@@ -3,49 +3,85 @@
 #include <string.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <errno.h>
+#include <pwd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
 int parseInput(char* args[], char* line);
 char* cmdExist(char* cmd, char* path);
+void doIoacct(pid_t pid);
 
 int main()
 {
 	system("clear");		/*	might not be allowed */
-	
+
 	char host_name[64];
 	char* user_name = (char* ) malloc(64);
 	char curr_dir[1024];
-	
+	char last_dir[1024];
+
 	char* args0 = (char* ) malloc(12);
 	char* args1 = (char* ) malloc(12);
 	char* args2 = (char* ) malloc(12);
 	char* args3 = (char* ) malloc(12);
 	char* args[] = {args0, args1, args2, args3, (char *)NULL};			// FIGURE THIS OUT!
 	char line[256];
-	
-	/* maybe put below 2 functions inside loop? */
-	gethostname(host_name, sizeof(host_name));
-	user_name = getlogin();
-	
+	int ioat;
+	int background;
+
+
 	while(1)
-	{	
+	{
+		ioat = 0;
+		background = 0;
 		args[0] = args[1] = args[2] = args[3] = NULL;
+		gethostname(host_name, sizeof(host_name));
+		user_name = getlogin();
 		getcwd(curr_dir, sizeof(curr_dir));
 		printf("%s@%s:%s $ ", host_name, user_name, curr_dir);
-		
+
 		fgets(line, sizeof(line), stdin);
 		int numArgs = parseInput(args, line);
 		if(numArgs == 0)
 			continue;
 		// printf("\n%s %s %s %s\n", args[0], args[1], args[2], args[3]);
 
-		if(strcmp(args[0], "cd") == 0 || strcmp(args[0], "ioacct") == 0 || strcmp(args[0], "exit") == 0) {
-			if (strcmp(args[0], "cd") == 0) {
-				chdir(args[1]);
+		if(strcmp(args[0], "ioacct") == 0) {
+			int i=0;
+			for(;i<numArgs;i++) {
+				args[i] = args[i+1];
 			}
-			else if (strcmp(args[0], "ioacct") == 0) {
-				// do ioacct
+			ioat = 1;
+		}
+
+		int i=3;
+		for(; i>=0; i--) {
+			if(args[i] != NULL) {
+				int last_len = strlen(args[i]);
+				if(args[i][last_len-1] == '&' && last_len > 1) {
+					args[i][last_len-1] = '\0';
+					background = 1; printf("BGBGBG");
+				}
+				else if(args[i][last_len-1] == '&' && last_len == 1) {
+					args[i] = NULL;
+					background = 1; printf("BGBGBG");
+				}
+				break;
+			}
+		} printf("\n%s %s %s %s\n", args[0], args[1], args[2], args[3]);
+
+		if(strcmp(args[0], "cd") == 0 || strcmp(args[0], "exit") == 0) {
+			if (strcmp(args[0], "cd") == 0) {
+				if(args[1] == NULL || strcmp(args[1], "~") == 0) {
+					struct passwd* pw = getpwuid(getuid());
+					chdir(pw->pw_dir);
+				}
+				else if(strcmp(args[1], "-") == 0)
+					chdir(last_dir);
+				else
+					chdir(args[1]);
+				strcpy(last_dir, curr_dir);
 			}
 			else if (strcmp(args[0], "exit") == 0) {
 				if(args[1] == NULL)
@@ -53,17 +89,19 @@ int main()
 				else
 					return atoi(args[1]);
 			}
+			if(ioat == 1)
+				doIoacct(getpid());
 		}
 		else {
 			// Searching for command here
-			
+
 			char* cmdPath = cmdExist(args[0], getenv("PATH"));
-			
+
 			// IF FOUND ------------------------------------------------
 			if(cmdPath != NULL) {
 				// printf("Command '%s' FOUND in %s !\n", args[0], cmdPath);
 				strcpy(args[0], cmdPath);
-				
+
 				pid_t child = fork();
 				if (child == 0) {
 					int rtn = execv(cmdPath, (char **)args);
@@ -72,26 +110,64 @@ int main()
 					// fork failed
 					exit(EXIT_FAILURE);
 				}
-				// else if & run background
+				else if (background == 1) {
+					printf("back!");
+					pid_t child_finished;
+					child_finished = waitpid(-1, (int *)NULL, WNOHANG);
+
+					if(ioat == 1)
+						doIoacct(child);	// <-- WEIRDDDDDDDDDD
+				}
 				else {
 					pid_t child_finished;
 					child_finished = waitpid(-1, (int *)NULL, 0);
+
+					if(ioat == 1)
+						doIoacct(child);
 				}
 			}
-			
+
 			// IF NOT FOUND --------------------------------------------
 			else {
 				printf("Command '%s' NOT FOUND!\n", args[0]);
 			}
 		}
-		
+
 		// freeing args memory accordingly
 		int x=0;
-		for(;x<numArgs-1; x++) {
-			free(args[x]); 
+		for(;x<numArgs-1; x++) {		// FIX THIS when 'man' then 'q' it fails
+			// printf("1");
+			free(args[x]);
+			// printf("2");
 		}
 	}
-}	
+}
+
+void doIoacct(pid_t pid)
+{
+	char pid_c[10];
+	char filename[] = "/proc/";
+	snprintf(pid_c, 10, "%d", (int)pid);
+	strcat(filename, pid_c);
+	strcat(filename, "/io");
+
+	printf("%s\n", filename);
+
+	FILE* fp;
+	fp = fopen(filename, "r");
+	if(fp == NULL) {
+		printf("Cant open io file at %s, errno: %d\n", filename, errno);
+	}
+	else {
+		char line[64];
+		int i=0;
+		for(;i<6; i++) {
+			fgets(line, sizeof(line), fp);
+			if(i == 4 || i == 5)
+				printf("%s", line);
+		}
+	}
+}
 
 int parseInput(char* args[], char* line)
 {
@@ -104,24 +180,24 @@ int parseInput(char* args[], char* line)
 	while(token = strtok(NULL, " ")) {
 		args[i] = strdup(token);
 		i++;
-	} 
-	size_t ln = strlen(args[i-1])-1; 
-	if(args[i-1][ln] == '\n') 
+	}
+	size_t ln = strlen(args[i-1])-1;
+	if(args[i-1][ln] == '\n')
 		args[i-1][ln] = '\0';
 	return i+1;
 }
 
-char* cmdExist(char* cmd, char* path) 
+char* cmdExist(char* cmd, char* path)
 {
-	DIR *d; 
+	DIR *d;
 	struct dirent *dir;
 	char* token;
 	char* cmdPath;
-	
+
 	// first path, strtok on 'path'
 	token = strtok(strdup(path), ":");
 	d = opendir(strdup(token));
-	if (d) 
+	if (d)
 	{
 		while ((dir = readdir(d)) != NULL)
 			if(strcmp(cmd, dir->d_name) == 0) {
@@ -132,7 +208,7 @@ char* cmdExist(char* cmd, char* path)
 			}
 		closedir(d); // ADD MORE THESE
 	}
-	
+
 	// rest of paths, strtok on 'NULL'
 	while(token = strtok(NULL, ":"))
 	{
