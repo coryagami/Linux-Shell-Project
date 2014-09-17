d5f53dceabce7bee67dd68e7b2ca69737885bf7c
OloE0pia <lores.javier@gmail.com>
2014-09-17 13:34:19 -0400

    Modularized the parsing

--- mainOld.c
+++ mainOld.c
@@ -5,225 +5,312 @@
 #include <dirent.h>
 #include <errno.h>
 #include <pwd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "arglist.h"
 
-int parseInput(char* args[], char* line);
-char* cmdExist(char* cmd, char* path);
-void doIoacct(pid_t pid);
+
+#define MAX_SIZE 256
+#define INIT_ARG_SIZE 5
+#define MAX_FILENAME_SIZE 64
+
+bool parseInput(arglist* arg_list, char* line);
+bool parseBG(arglist* arg_list);
+bool parseInRedir(arglist* arg_list, char* redir_file);
+bool parseOutRedir(arglist* arg_list, char* redir_file);
+bool parseAppendRedir(arglist* arg_list, char* redir_file);
+bool parseIoacct(arglist* arg_list);
+char* getCmdPath(char* cmd, char* path);
+void executeIoacct(pid_t pid, int* read_bytes, int* write_bytes);
 
 int main()
 {
-	system("clear");		/*	might not be allowed */
+	char host_name[MAX_SIZE];
+	char* user_name = NULL;
+	char* curr_dir = NULL;
+	char* last_dir = NULL;
 
-	char host_name[64];
-	char* user_name = (char* ) malloc(64);
-	char curr_dir[1024];
-	char last_dir[1024];
+	char line[MAX_SIZE];
+	char redir_file[MAX_FILENAME_SIZE];
+	arglist arg_list;
 
-	char* args0 = (char* ) malloc(12);
-	char* args1 = (char* ) malloc(12);
-	char* args2 = (char* ) malloc(12);
-	char* args3 = (char* ) malloc(12);
-	char* args[] = {args0, args1, args2, args3, (char *)NULL};			// FIGURE THIS OUT!
-	char line[256];
-	int ioat;
-	int background;
+	int read_bytes = 0;
+	int write_bytes = 0;
 
+	// Flags
+        bool is_ioacct_cmd;
+        bool is_background_process;
+        bool is_in_redir;
+        bool is_out_redir;
+        bool is_append_redir;
 
 	while(1)
 	{
-		ioat = 0;
-		background = 0;
-		args[0] = args[1] = args[2] = args[3] = NULL;
+		// Print prompt
 		gethostname(host_name, sizeof(host_name));
 		user_name = getlogin();
-		getcwd(curr_dir, sizeof(curr_dir));
+//		user_name = getpwuid(getuid())->pw_name;
+		curr_dir = getcwd(NULL, 0);
 		printf("%s@%s:%s $ ", host_name, user_name, curr_dir);
 
+		// Parse input
 		fgets(line, sizeof(line), stdin);
-		int numArgs = parseInput(args, line);
-		if(numArgs == 0)
+		argListCreate(&arg_list, INIT_ARG_SIZE);
+		if (!parseInput(&arg_list, line))
 			continue;
-		// printf("\n%s %s %s %s\n", args[0], args[1], args[2], args[3]);
 
-		if(strcmp(args[0], "ioacct") == 0) {
-			int i=0;
-			for(;i<numArgs;i++) {
-				args[i] = args[i+1];
+		is_background_process = parseBG(&arg_list);
+		is_in_redir = parseInRedir(&arg_list, redir_file);
+		is_out_redir = parseOutRedir(&arg_list, redir_file);
+		is_append_redir = parseAppendRedir(&arg_list, redir_file);
+		is_ioacct_cmd = parseIoacct(&arg_list);
+
+		if(strcmp(arg_list.args[0], "exit") == 0)
+			return (arg_list.size == 1) ? 0 : atoi(arg_list.args[1]);
+
+		if(strcmp(arg_list.args[0], "cd") == 0 && !is_background_process)
+		{
+			if(arg_list.size == 1 || strcmp(arg_list.args[1], "~") == 0)
+				chdir(getpwuid(getuid())->pw_dir);
+			else if(strcmp(arg_list.args[1], "-") == 0)
+				chdir(last_dir);
+			else
+				chdir(arg_list.args[1]);
+
+			if(last_dir != NULL) free(last_dir);
+			last_dir = curr_dir;
+
+			if(is_ioacct_cmd)
+			{
+				printf("bytes read: -1\n");
+				printf("bytes written: -1\n");
 			}
-			ioat = 1;
 		}
+		else
+		{
 
-		int i=3;
-		for(; i>=0; i--) {
-			if(args[i] != NULL) {
-				int last_len = strlen(args[i]);
-				if(args[i][last_len-1] == '&' && last_len > 1) {
-					args[i][last_len-1] = '\0';
-					background = 1; printf("BGBGBG");
-				}
-				else if(args[i][last_len-1] == '&' && last_len == 1) {
-					args[i] = NULL;
-					background = 1; printf("BGBGBG");
-				}
-				break;
-			}
-		} printf("\n%s %s %s %s\n", args[0], args[1], args[2], args[3]);
+			char* cmdPath = getCmdPath(arg_list.args[0], getenv("PATH"));
 
-		if(strcmp(args[0], "cd") == 0 || strcmp(args[0], "exit") == 0) {
-			if (strcmp(args[0], "cd") == 0) {
-				if(args[1] == NULL || strcmp(args[1], "~") == 0) {
-					struct passwd* pw = getpwuid(getuid());
-					chdir(pw->pw_dir);
-				}
-				else if(strcmp(args[1], "-") == 0)
-					chdir(last_dir);
-				else
-					chdir(args[1]);
-				strcpy(last_dir, curr_dir);
-			}
-			else if (strcmp(args[0], "exit") == 0) {
-				if(args[1] == NULL)
-					return 0;
-				else
-					return atoi(args[1]);
-			}
-			if(ioat == 1)
-				doIoacct(getpid());
-		}
-		else {
-			// Searching for command here
+			if (cmdPath != NULL)
+			{
+				argListRemove(&arg_list, 0);
+				argListAdd(&arg_list, cmdPath, 0);
 
-			char* cmdPath = cmdExist(args[0], getenv("PATH"));
-
-			// IF FOUND ------------------------------------------------
-			if(cmdPath != NULL) {
-				// printf("Command '%s' FOUND in %s !\n", args[0], cmdPath);
-				strcpy(args[0], cmdPath);
-
+				// To catch/cleanup old background processes
+				pid_t child_finished = waitpid(-1, (int *)NULL, WNOHANG);
 				pid_t child = fork();
-				if (child == 0) {
-					int rtn = execv(cmdPath, (char **)args);
+
+				if (child == 0)
+				{
+					fflush(0);
+
+					// Input redirect
+					if (is_in_redir)
+					{
+						int fd = open(redir_file, O_RDONLY);
+						dup2(fd, STDIN_FILENO);
+						close(fd);
+					}
+
+					// Output redirect
+					if (is_out_redir)
+					{
+						int fd = creat(redir_file, 0644);
+						dup2(fd, STDOUT_FILENO);
+						close(fd);
+					}
+
+					// Append redirect
+					if (is_append_redir)
+					{
+						int fd = open(redir_file, O_WRONLY|O_APPEND);
+						dup2(fd, STDOUT_FILENO);
+						close(fd);
+					}
+
+					int rtn = execv(cmdPath, arg_list.args);
 				}
-				else if (child < 0) {
-					// fork failed
+				else if (child < 0)
+				{
 					exit(EXIT_FAILURE);
 				}
-				else if (background == 1) {
-					printf("back!");
-					pid_t child_finished;
-					child_finished = waitpid(-1, (int *)NULL, WNOHANG);
+				else if (is_background_process)
+				{
+					pid_t child_finished = waitpid(-1, (int *)NULL, WNOHANG);
 
-					if(ioat == 1)
-						doIoacct(child);	// <-- WEIRDDDDDDDDDD
+					// if(is_ioacct_cmd) executeIoacct(child, &read_bytes, &write_bytes);
 				}
-				else {
-					pid_t child_finished;
-					child_finished = waitpid(-1, (int *)NULL, 0);
+				else if (is_ioacct_cmd)
+				{
+					while(waitpid(-1, (int *)NULL, WNOHANG) == 0)
+						executeIoacct(child, &read_bytes, &write_bytes);
 
-					if(ioat == 1)
-						doIoacct(child);
+					printf("bytes read: %d\n", read_bytes);
+					printf("bytes written: %d\n", write_bytes);
+				}
+				else
+				{
+					pid_t child_finished = waitpid(-1, (int *)NULL, 0);
 				}
 			}
-
-			// IF NOT FOUND --------------------------------------------
-			else {
-				printf("Command '%s' NOT FOUND!\n", args[0]);
+			else
+			{
+				if (strcmp(arg_list.args[0], "cd") != 0)
+					printf("Command '%s' NOT FOUND!\n", arg_list.args[0]);
 			}
 		}
 
-		// freeing args memory accordingly
-		int x=0;
-		for(;x<numArgs-1; x++) {		// FIX THIS when 'man' then 'q' it fails
-			// printf("1");
-			free(args[x]);
-			// printf("2");
-		}
+		// Memory clean up
+		argListDestroy(&arg_list);
+
 	}
+	return 0;
 }
 
-void doIoacct(pid_t pid)
+bool parseInput(arglist* arg_list, char* line)
 {
-	char pid_c[10];
-	char filename[] = "/proc/";
-	snprintf(pid_c, 10, "%d", (int)pid);
-	strcat(filename, pid_c);
-	strcat(filename, "/io");
+	char* token = strtok(line, " \n");
 
-	printf("%s\n", filename);
+	while(token)
+	{
+		argListAdd(arg_list, token, arg_list->size);
+		token = strtok(NULL, " \n");
+	}
 
-	FILE* fp;
-	fp = fopen(filename, "r");
-	if(fp == NULL) {
-		printf("Cant open io file at %s, errno: %d\n", filename, errno);
+	// Error checking
+	if (arg_list->size == 0) return false;
+	if (arg_list->size == 1 && strcmp(arg_list->args[0], "ioacct") == 0)
+	{
+		printf("Usage: ioacct <command>\n");
+		return false;
 	}
-	else {
-		char line[64];
-		int i=0;
-		for(;i<6; i++) {
-			fgets(line, sizeof(line), fp);
-			if(i == 4 || i == 5)
-				printf("%s", line);
-		}
-	}
+
+	return true;
 }
 
-int parseInput(char* args[], char* line)
+bool parseBG(arglist* arg_list)
 {
-	char* token;
-	token = strtok(line, " \n");
-	if(token == NULL)
-		return 0;
-	args[0] = strdup(token);
-	int i=1;
-	while(token = strtok(NULL, " ")) {
-		args[i] = strdup(token);
-		i++;
+        size_t last_arg_len = strlen(arg_list->args[arg_list->size-1]);
+	if(arg_list->args[arg_list->size-1][last_arg_len-1] == '&')
+	{
+		if (last_arg_len > 1)
+			arg_list->args[arg_list->size-1][last_arg_len-1] = '\0';
+		else if(last_arg_len == 1)
+			argListRemove(arg_list, arg_list->size-1);
+
+		return true;
 	}
-	size_t ln = strlen(args[i-1])-1;
-	if(args[i-1][ln] == '\n')
-		args[i-1][ln] = '\0';
-	return i+1;
+	return false;
 }
 
-char* cmdExist(char* cmd, char* path)
+bool parseInRedir(arglist* arg_list, char* redir_file)
+{
+	if (arg_list->size >= 3 && strcmp(arg_list->args[arg_list->size-2], "<") == 0)
+	{
+		strcpy(redir_file, arg_list->args[arg_list->size-1]);
+		argListRemove(arg_list, arg_list->size-1);
+		argListRemove(arg_list, arg_list->size-1);
+		return true;
+	}
+	return false;
+}
+
+bool parseOutRedir(arglist* arg_list, char* redir_file)
+{
+	if (arg_list->size >= 3 && strcmp(arg_list->args[arg_list->size-2], ">") == 0)
+	{
+		strcpy(redir_file, arg_list->args[arg_list->size-1]);
+		argListRemove(arg_list, arg_list->size-1);
+		argListRemove(arg_list, arg_list->size-1);
+		return true;
+	}
+	return false;
+}
+
+bool parseAppendRedir(arglist* arg_list, char* redir_file)
+{
+	if (arg_list->size >= 3 && strcmp(arg_list->args[arg_list->size-2], ">>") == 0)
+	{
+		strcpy(redir_file, arg_list->args[arg_list->size-1]);
+		argListRemove(arg_list, arg_list->size-1);
+		argListRemove(arg_list, arg_list->size-1);
+		return true;
+	}
+	return false;
+}
+
+bool parseIoacct(arglist* arg_list)
+{
+	if (strcmp(arg_list->args[0], "ioacct") == 0)
+	{
+		argListRemove(arg_list, 0);
+		return true;
+	}
+	return false;
+}
+
+
+char* getCmdPath(char* cmd, char* path)
 {
 	DIR *d;
 	struct dirent *dir;
 	char* token;
 	char* cmdPath;
+	path = strdup(path);
 
-	// first path, strtok on 'path'
-	token = strtok(strdup(path), ":");
-	d = opendir(strdup(token));
-	if (d)
-	{
-		while ((dir = readdir(d)) != NULL)
-			if(strcmp(cmd, dir->d_name) == 0) {
-				cmdPath = token;
-				strcat(cmdPath, "/");
-				closedir(d);
-				return strcat(cmdPath, cmd);
-			}
-		closedir(d); // ADD MORE THESE
-	}
-
-	// rest of paths, strtok on 'NULL'
-	while(token = strtok(NULL, ":"))
-	{
-		d = opendir(strdup(token));
-		if(d)
+	token = strtok(path, ":");
+        while(token)
+        {
+                d = opendir(token);
+                if(d)
 		{
-			while ((dir = readdir(d)) != NULL)
-				if(strcmp(cmd, dir->d_name) == 0) {
-					cmdPath = token;
-					strcat(cmdPath, "/");
+                        while ((dir = readdir(d)) != NULL)
+                                if(strcmp(cmd, dir->d_name) == 0)
+				{
+                                        cmdPath = token;
+                                        strcat(cmdPath, "/");
+                                        strcat(cmdPath, cmd);
 					closedir(d);
-					return strcat(cmdPath, cmd);
-				}
-		}
+					return cmdPath;
+                                }
+                }
+		closedir(d);
+		token = strtok(NULL, ":");
 	}
-	closedir(d);
+	free(path);
 	return NULL;
+}
+
+void executeIoacct(pid_t pid, int* read_bytes, int* write_bytes)
+{
+	char filename[MAX_FILENAME_SIZE];
+	sprintf(filename, "/proc/%d/io", (int)pid);
+
+	FILE* fp = fopen(filename, "r");
+	if(fp == NULL)
+	{
+	}
+        else
+	{
+                char line[MAX_SIZE];
+                int i = 0;
+                for(;i < 2; i++)
+		{
+                        fgets(line, sizeof(line), fp);
+                        if(i == 0 || i == 1)
+			{
+				char* byte_data = strtok(line, " \n");
+				byte_data = strtok(NULL, " \n");
+
+				if(i == 0)
+					*read_bytes = atoi(byte_data);
+				else if(i == 1)
+					*write_bytes = atoi(byte_data);
+			}
+                }
+        }
 }
